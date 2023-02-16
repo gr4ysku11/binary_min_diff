c6bea4f8cfbc933d3d616a74c3fe127b497e6732
gr4ysku11 <gr4ysku11@protonmail.com>
2023-02-16 04:19:50 -0800

    pre flight checks

--- binary_min_diff.py
+++ binary_min_diff.py
@@ -2,8 +2,10 @@
 
 import sys
 
-# TODO
 # exit if two args not passed in via argv
+if sys.argv.count != 2:
+    print("[USAGE] binary_min_diff.py <original file> <fuzzed crash file>")
+    exit()
 
 orig_file = open(sys.argv[1], 'rb')
 fuzz_file = open(sys.argv[2], 'rb')
@@ -16,6 +18,11 @@ ext = orig_file.name[ext_i+1:]
 
 orig_bytes = orig_file.read()
 fuzz_bytes = fuzz_file.read()
+
+# abort if file sizes differ
+if (len(orig_bytes != fuzz_bytes)):
+    print("[ABORT] file lengths do not match!")
+    exit()
 
 # loop over byte arrays and detect differences
 i = 0
